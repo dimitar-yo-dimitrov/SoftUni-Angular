263d0415507ab70884e96baf99d08bbd1e66e5a1
Dimitar Dimitrov <81187988+dimitar-yo-dimitrov@users.noreply.github.com>
2023-06-28 12:50:42 +0300

    01 and 02 problems has been completed

---
+++ TypeScript-Playground-Lab/02-Tickets.ts
@@ -0,0 +1,43 @@
+class Tickets {
+  destination: string;
+  price: number;
+  status: string;
+
+  constructor(destination: string, price: number, status: string) {
+    this.destination = destination;
+    this.price = price;
+    this.status = status;
+  }
+}
+
+function dataForTicket(array: string[], criteria: string) {
+  const sortedTickets = array
+    .map((string) => string.split("|"))
+    .map(
+      ([destination, price, status]) =>
+        new Tickets(destination, Number(price), status)
+    )
+    .sort((a, b) => a[criteria].localeCompare(b[criteria]));
+
+  console.log(sortedTickets);
+  return sortedTickets;
+}
+
+dataForTicket(
+  [
+    "Philadelphia|94.20|available",
+    "New York City|95.99|available",
+    "New York City|95.99|sold",
+    "Boston|126.20|departed",
+  ],
+  "destination"
+);
+dataForTicket(
+  [
+    "Philadelphia|94.20|available",
+    "New York City|95.99|available",
+    "New York City|95.99|sold",
+    "Boston|126.20|departed",
+  ],
+  "status"
+);
